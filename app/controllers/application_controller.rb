cea6ed64a9d8bd2b44f0e54ce0c65f644486de67
EC2 Default User <ec2-user@ip-172-31-9-131.ap-northeast-1.compute.internal>
2022-09-21 17:57:25 +0000

    [Add]navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+  
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
