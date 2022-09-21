cb46419b0128b59a19961137d0e72668f8e447db
EC2 Default User <ec2-user@ip-172-31-9-131.ap-northeast-1.compute.internal>
2022-09-21 20:03:35 +0000

    [Add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
-  
+
+  def uodate
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+  def index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introducshon)
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def get_image
-    unless image.attached?
+  def get_profile_image(width, height)
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      iage.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [100, 100]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
